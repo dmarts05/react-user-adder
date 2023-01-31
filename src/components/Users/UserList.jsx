9afc8acb8a078bf32abb1011f45c6dc480a529d7
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-01-31 02:34:19 +0100

    Add more responsiveness to UserList

--- src/components/Users/UserList.jsx
+++ src/components/Users/UserList.jsx
@@ -2,7 +2,7 @@ import UserCard from './UserCard';
 
 export default function UserList(props) {
   return (
-    <ul className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
+    <ul className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6'>
       {props.users.map((user) => (
         <UserCard key={user.id} username={user.username} age={user.age} />
       ))}
