88424a2662ff3669dd33a4853db774718662f65d
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-01-31 01:38:13 +0100

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Users from './components/Users/Users';
 export default function App() {
   return (
     <div className='flex h-screen w-screen flex-col flex-wrap content-center gap-8 bg-zinc-900 py-8'>
+      <h1 className='w-fit self-center rounded-lg bg-white p-4 text-center text-4xl font-semibold'>
+        User Adder
+      </h1>
       <Card>
         <AddUserForm />
       </Card>
